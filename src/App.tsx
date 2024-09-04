2ba6708fbec38055cf2ba6070f6a5235845e2e93
alena-makouskaya <elena.makovskaya.br@gmail.com>
2024-09-04 07:05:50 +0300

    01

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,38 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      This is App Component
+
+      <Rating />
+      <Accordion />
     </div>
   );
+}
+
+function Rating() {
+  return (
+    <div>
+      <div>Star </div>
+      <div>Star </div>
+      <div>Star </div>
+      <div>Star </div>
+      <div>Star </div>
+    </div>
+  )
+}
+
+function Accordion() {
+  return (
+    <div>
+      <h3>Menu</h3>
+
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  )
 }
 
 export default App;
